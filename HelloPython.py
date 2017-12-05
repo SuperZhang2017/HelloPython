88823d24d03ea17766f3cc60ead3d4016700182e
SuperZhang2017 <13126735876@163.com>
2017-12-06 00:36:41 +0800

    hi

--- HelloPython.py
+++ HelloPython.py
@@ -1,8 +1,20 @@
 #1.console output
 s = '123'
 print(s)
-#2.python basic
-str = 'Hello Python'
-#3.create string
-s=str(range(5))
+#2.create string
+str=str(range(5))
 print(s)
+a=len(s)
+#3.visit string
+s[0]
+print(s[0])
+strFirst = "Hello Python"
+print(strFirst[4])
+print(strFirst[6:])
+#"He"
+print(strFirst[:2])
+#"Hello Python"
+print(strFirst[::])
+#"Hello Python"
+print(strFirst[:])
+print("hi")
