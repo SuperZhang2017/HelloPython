606beb2473f61b1bf3862bf9f4dca23fa6dd55c1
SuperZhang2017 <13126735876@163.com>
2017-12-06 23:04:23 +0800

    day two practise

--- HelloPython.py
+++ HelloPython.py
@@ -17,4 +17,9 @@ print(strFirst[:2])
 print(strFirst[::])
 #"Hello Python"
 print(strFirst[:])
-
+#程序输入
+user = input('please enter you name:')
+print('my name is:'+user)
+print("%s is number %d"%("python",1))
+print(type(14))
+print(type(None))
